e0b2699824c012687fb56e46a3553ea3481c3c24
D-Maysena <yassercomunica@gmail.com>
2024-06-02 18:37:00 -0600

    Cube with lighting�

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame as pg
 import moderngl as mgl
 import sys
+from model import *
+from camera import Camera
+from light import Light
 
 class GraphicsEngine:
     #El método __init__(self, win_size=(800, 600)) en la clase GraphicsEngine es el 
@@ -24,23 +27,41 @@ class GraphicsEngine:
         #OPENGL indica que se utilizará OpenGL para renderizar en la ventana, mientras que DOUBLEBUF habilita el doble búfer, lo que reduce el parpadeo al renderizar.
         pg.display.set_mode(self.WIN_SIZE, flags=pg.OPENGL | pg.DOUBLEBUF)
     
+        #Ajustes del mouse
+        # captura (o "agarra") el cursor del ratón dentro de la ventana de Pygame. Esto significa que el cursor no podrá salir de la ventana mientras esta esté activa
+        pg.event.set_grab(True)
+        pg.mouse.set_visible(False)
+    
+    
         #Crea un contexto de OpenGL utilizando ModernGL, proporcionando un entorno para 
         #realizar operaciones gráficas con OpenGL en la aplicación, como renderizado de objetos, 
         # configuración de shaders y manipulación de texturas.
         self.ctx = mgl.create_context()
-    
+        #Prueba de Profundidad, asegurando el ordenamiento correcto de los objetos en la escena 3D, y el Descarte de Caras, optimizando el renderizado al evitar dibujar caras no visibles.
+        self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE)
        #crea un objeto de reloj pygame, permitiendo controlar el tiempo dentro del juego, 
        # como limitar la velocidad de fotogramas o calcular el tiempo transcurrido entre 
        # fotogramas para mantener una animación suave y consistente
         self.clock = pg.time.Clock()
+        self.time = 0
+        self.delta_time = 0
+        #Light
+        self.light = Light()
+        #Camara
+        self.camera = Camera(self)
+        #Escene
+        #crea una instancia de la clase Triangle y la asigna a la variable self.scene
+        self.scene = Cube(self)
         
     def check_events(self):
         # Gestiona los eventos de entrada del usuario, permitiendo que 
         # la aplicación responda a acciones como cerrar la ventana.
         #Los eventos son almacenados en una cola
         for event in pg.event.get():
-            print(event.type)
+            #print(event.type)
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                #Cuando cerramos el programa liberamos memroai
+                self.scene.destroy()
                 pg.quit()
                 sys.exit()
         
@@ -48,21 +69,29 @@ class GraphicsEngine:
     # luego actualizarla para mostrar los cambios realizados en la escena gráfica    
     def render(self):
         #limpiamos la ventana y le damos un color nuevo a traves del contexto de opengl
-        self.ctx.clear(color=(0.8, 0.6, 0.18))
+        self.ctx.clear(color=(0.0, 0.0, 0))
+        #Una vez que se cargo la ventana con el color, renderizamos el objeto
+        self.scene.render()
         #Luego actualizamos la ventana de pygame con flip
         pg.display.flip()
+        
+            
+    def get_time(self):
+        self.time = pg.time.get_ticks() * 0.001     
             
     # bucle principal de la aplicación. Similiar a una función main
     def run(self):
-        #Bcule infinito
+        #Bcule infinito 
         while True:
+            self.get_time()
             #Se llama al metodo check events
             self.check_events()
+            self.camera.update()
             #Al render para cambiar el color
             self.render()
             #A traves de clock que nos permite controlar el tiempo de nuestro juego
             #Limitamos los fps con tick
-            self.clock.tick(60)
+            self.delta_time =  self.clock.tick(60)
                 
 if __name__ == '__main__':
     # crea una instancia de la clase GraphicsEngine
---
+++ shaders/default.frag
@@ -0,0 +1,48 @@
+#version 330 core
+
+layout (location = 0) out vec4 fragColor;
+
+in vec2 uv_0;
+in vec3 normal;
+in vec3 fragPos;
+
+struct Light {
+    vec3 position;
+    vec3 Ia;
+    vec3 Id;
+    vec3 Is;
+};
+
+uniform Light light;
+
+//sampler2d para representar datos 2d
+uniform sampler2D u_texture_0;
+uniform vec3 camPos;
+
+vec3 getLight(vec3 color){
+    vec3 Normal = normalize(normal);
+    //Luz de ambiente
+    vec3 ambient = light.Ia;
+
+    vec3 lightDir = normalize(light.position - fragPos);
+    float diff = max(0, dot(lightDir, Normal));
+    vec3 diffuse = diff * light.Id;
+
+    vec3 viewDir = normalize(camPos - fragPos);
+    vec3 reflectDir = reflect(-lightDir, Normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0), 32);
+    vec3 specular = spec * light.Is;
+
+    return color * (ambient + diffuse + specular );
+}
+
+
+void main() 
+{
+    //toma un color predefinido y lo asigna al atributo de salida fragColor, que se utilizará para determinar 
+    //el color final de cada fragmento en la pantalla.
+    //vec3 color = vec3(uv_0, 0);  // Rojo
+    vec3 color = texture(u_texture_0, uv_0).rgb;
+    color = getLight(color);
+    fragColor = vec4(color, 1.0); // Asigna el color al fragmento
+}
