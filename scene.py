d3a0b75191b8dde198ef5f0d88f1fb60dba13099
Rugby27 <jsueburgos@gmail.com>
2024-06-08 22:22:21 -0600

    Test multiple lights in the scene

--- scene.py
+++ scene.py
@@ -1,41 +1,45 @@
 from model import *
+from light import Light, AdditionalLight, AdditionalLight2
 
 class Scene:
     def __init__(self, app):
         self.app = app
-        self.objects = []   
+        self.objects = []
+        self.lights = []
         self.load()
-         # skybox
         self.skybox = AdvancedSkyBox(app)
-    
+
     def add_object(self, obj):
         self.objects.append(obj)
-    
+
+    def add_light(self, light):
+        self.lights.append(light)
 
     def load(self):
         app = self.app
         add = self.add_object
-    
-        n, s = 80,2
+
+        n, s = 80, 2
         for x in range(-n, n, s):
-           for z in range (-n, n, s):
-              add(Cube(app, pos=(x, -s, z)))
-        
-        add(Coliseo(app, pos=(-20,-2, -10), scale=(0.002, 0.002, 0.002)))
-        add(Eiffel(app, pos=(20,-2, -10), scale=(0.0006, 0.0006, 0.0006)))
-        #add(Cube(app, pos=(50,-2, -10), scale=(10,10,10)))
-        
+            for z in range(-n, n, s):
+                add(Cube(app, pos=(x, -s, z)))
+
+        add(Coliseo(app, pos=(-20, -2, -10), scale=(0.002, 0.002, 0.002)))
+        add(Eiffel(app, pos=(20, -2, -10), scale=(0.0006, 0.0006, 0.0006)))
         add(PisaTower(app, pos=(0, -1, -5)))
-        
         add(Catedral(app, pos=(10, -2, -5)))
         add(Estatua(app, pos=(0, 10, -10)))
         add(Estatua2(app, pos=(5, 10, -10)))
-        
         add(Bigben(app, pos=(0, -2, -10)))
-        add(moai(app,pos=(-4,-2,-10)))
-        
-    
+        add(moai(app, pos=(-4, -2, -10)))
+
+        # Add lights
+        self.add_light(self.app.light)
+        self.add_light(AdditionalLight(position=(0, -3, -10), color=(1.0, 0.0, 0.0)))
+        self.add_light(AdditionalLight2(position=(0, 20, -10), color=(1, 0.5, 0.5)))
+
     def render(self):
-        for obj in self.objects: 
+        for obj in self.objects:
             obj.render()
-            self.skybox.render()
+        self.skybox.render()
+
--- shaders/default.frag
+++ shaders/default.frag
@@ -5,6 +5,7 @@ layout (location = 0) out vec4 fragColor;
 in vec2 uv_0;
 in vec3 normal;
 in vec3 fragPos;
+
 struct Light {
     vec3 position;
     vec3 Ia;
@@ -13,42 +14,40 @@ struct Light {
 };
 
 uniform Light light;
+uniform Light additionalLight1;
+uniform Light additionalLight2;
 
-//sampler2d para representar datos 2d
 uniform sampler2D u_texture_0;
 uniform vec3 camPos;
 
-vec3 getLight(vec3 color){
+vec3 getLight(vec3 color, Light light) {
     vec3 Normal = normalize(normal);
-    //Luz de ambiente
     vec3 ambient = light.Ia;
-
     vec3 lightDir = normalize(light.position - fragPos);
     float diff = max(0, dot(lightDir, Normal));
     vec3 diffuse = diff * light.Id;
     vec3 viewDir = normalize(camPos - fragPos);
     vec3 reflectDir = reflect(-lightDir, Normal);
-    
     float spec = pow(max(dot(viewDir, reflectDir), 0), 32);
     vec3 specular = spec * light.Is;
-
-    return color * (ambient + diffuse + specular );
+    return color * (ambient + diffuse + specular);
 }
 
-void main() 
-{
+void main() {
     float gamma = 2.2;
-    //toma un color predefinido y lo asigna al atributo de salida fragColor, que se utilizará para determinar 
-    //el color final de cada fragmento en la pantalla.
-    //vec3 color = vec3(uv_0, 0);  // Rojo
     vec3 color = texture(u_texture_0, uv_0).rgb;
-    
     color = pow(color, vec3(gamma));
-    
-    color = getLight(color);
-    color = pow(color, 1 / vec3(gamma));
-    fragColor = vec4(color, 1.0); // Asigna el color al fragmento
+
+    vec3 resultColor = getLight(color, light);
+    resultColor += getLight(color, additionalLight1);
+    resultColor += getLight(color, additionalLight2);
+
+    resultColor = pow(resultColor, 1.0 / vec3(gamma));
+    fragColor = vec4(resultColor, 1.0);
 }
+
+
+
 
 
 
